1e2d646826f184c372ec2ec06f62dd7be3026dc6
Chris Arsenault <carsena2@gmail.com>
2025-10-21 23:32:02 -0700

    remove nat and hopefully prevent oscilation as well

--- infrastructure/terraform/vpc.tf
+++ infrastructure/terraform/vpc.tf
@@ -188,11 +188,6 @@ resource "aws_route" "private_to_wg_via_wg" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
-
   tags = { Name = "${local.prefix}-private-rt" }
 }
 
@@ -200,21 +195,27 @@ resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
+#
+# resource "aws_eip" "nat" {
+#   domain = "vpc"
+#
+#   tags = {
+#     Name = "${local.prefix}-nat-eip"
+#   }
+# }
+#
+# resource "aws_nat_gateway" "this" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public.id
+#   depends_on    = [aws_internet_gateway.this]
+#
+#   tags = {
+#     Name = "${local.prefix}-nat"
+#   }
+# }
 
-resource "aws_eip" "nat" {
-  domain = "vpc"
-
-  tags = {
-    Name = "${local.prefix}-nat-eip"
-  }
-}
-
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-  depends_on    = [aws_internet_gateway.this]
-
-  tags = {
-    Name = "${local.prefix}-nat"
-  }
-}
+# resource "aws_route" "private_default_via_nat" {
+#   route_table_id         = aws_route_table.private.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.this.id
+# }
