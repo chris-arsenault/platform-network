62168e62354aad91ec8551d777c905bc1f0c632b
Chris Arsenault <carsena2@gmail.com>
2025-10-23 11:44:41 -0700

    add ssm to nat

--- infrastructure/terraform/ec2-nat.tf
+++ infrastructure/terraform/ec2-nat.tf
@@ -26,11 +26,26 @@ resource "aws_security_group" "nat" {
   }
 }
 
+resource "aws_iam_role" "nat" {
+  name_prefix        = "${local.prefix}-nat-"
+  assume_role_policy = data.aws_iam_policy_document.assume_ec2.json
+}
+
+resource "aws_iam_instance_profile" "nat" {
+  name_prefix = "${local.prefix}-nat-"
+  role        = aws_iam_role.nat.name
+}
+
+resource "aws_iam_role_policy_attachment" "nat_ssm" {
+  role       = aws_iam_role.nat.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 module "nat" {
   source = "./modules/ec2_instance"
 
   name                 = "${local.prefix}-nat"
-  iam_instance_profile = null
+  iam_instance_profile = aws_iam_instance_profile.nat.name
   subnet_id            = aws_subnet.public.id
   security_group_ids   = [aws_security_group.nat.id]
   associate_eip        = true
